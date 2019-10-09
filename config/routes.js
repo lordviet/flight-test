099e6608a9583151d6effb911055a923e008e6cb
superfan <trendemil@gmail.com>
2019-10-09 23:40:27 +0300

    added registration and login

--- config/routes.js
+++ config/routes.js
@@ -3,6 +3,12 @@ const controller = require('../controllers/flightTest-controller');
 module.exports = (app) => {
     app.get("/", controller.getIndex);
     app.get("/question", controller.getQuestion);
+
+    app.get("/register", controller.getRegister);
+    app.post("/register", controller.postRegister);
+
+    app.get("/login", controller.getLogin);
+    app.post("/login", controller.postLogin);
     // this route is for testing only
     // app.get("/question/:id", controller);
     // another testing route
