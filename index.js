8cc7032c19551f38a47d5d8f81af99addc87206d
superfan <trendemil@gmail.com>
2019-10-10 15:38:49 +0300

    added new packages and login/register in home view

--- index.js
+++ index.js
@@ -10,14 +10,3 @@ dbConnector().then(() => {
 
     app.listen(config.port, console.log(`Listening on port ${config.port}`));
 }).catch(console.error);
-
-
-
-
-// const config = require('./config/config')[env];
-// const app = require('express')();
-// require('./config/express')(app);
-// require('./config/routes')(app);
-
-
-// app.listen(config.port, console.log(`Listening on port ${config.port}!`));
