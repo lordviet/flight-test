bf46f1c7526984fdebc4eab9b3c8217a2592a193
lordviet <viet1231@abv.bg>
2019-10-11 04:02:09 +0300

    added score funcionality

--- static/scripts/question.js
+++ static/scripts/question.js
@@ -1,29 +1,51 @@
+let questions = [...document.getElementsByClassName("questionInfo")];
+const numberOfQuestions = questions.length;
+let correctAnswers = 0;
+let numberOfClicks = 0;
+
 const checkCorrectness = (event) => {
     const clickedAnswer = event.target;
     const answerIsTrue = clickedAnswer.id === "true" ? true : false;
     const questionId = $(event.target).parent().parent()[0].id;
 
+    numberOfClicks++;
+
     if (answerIsTrue) {
         $(clickedAnswer).children().css("color", "green");
+        correctAnswers++;
     }
-    else{
+    else {
         $(clickedAnswer).children().css("color", "red");
         $(clickedAnswer).siblings("#true").css("color", "green");
     }
 
-    $(`#${questionId} div.answer`)
-        .toArray()
-        .forEach(question => {
-            $(question).unbind("click", checkCorrectness);
-        });
+    if (allQuestionsAreAnswered(numberOfQuestions, numberOfClicks)) {
+        showFinalScore(correctAnswers, numberOfQuestions);
+    }
+    removeEventListeners(questionId);
 
+    // Show explanation of the answer
     $(`#${questionId} #explanation`).css("display", "block");
 
 }
 
-let questions = [...document.getElementsByClassName("questionInfo")];
 
 for (let question of questions) {
     $(`#${question.id} div.answer`).on("click", checkCorrectness);
 }
 
+function removeEventListeners(questionId) {
+    $(`#${questionId} div.answer`)
+        .toArray()
+        .forEach(question => {
+            $(question).unbind("click", checkCorrectness);
+        });
+}
+
+function allQuestionsAreAnswered(numberOfQuestions, numberOfClicks) {
+    return numberOfQuestions === numberOfClicks;
+}
+
+function showFinalScore(correctAnswers, numberOfQuestions) {
+    $("h3.score").text(`You have ${correctAnswers} correct answers out of ${numberOfQuestions} questions`);
+}
