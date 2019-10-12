98f1bbc7e3eac58246e0e2409764a8451e74d172
lordviet <viet1231@abv.bg>
2019-10-12 03:47:30 +0300

    added verifyToken function in jwt

--- utils/jwt.js
+++ utils/jwt.js
@@ -5,6 +5,16 @@ function createToken(data) {
     return jwt.sign(data, secret, { expiresIn: "2d" });
 }
 
+function verifyToken(token) {
+    return new Promise((resolve, reject) => {
+        jwt.verify(token, secret, (err, data) => {
+            if (err) { reject(err); return; }
+            resolve(data);
+        });
+    });
+}
+
 module.exports = {
-    createToken
+    createToken,
+    verifyToken
 };
