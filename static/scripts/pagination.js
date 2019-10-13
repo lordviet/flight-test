a1b0bef4c1acc1e014cb10a7b943664dbaab548b
lordviet <viet1231@abv.bg>
2019-10-14 02:11:49 +0300

    completed the pagination logic

--- static/scripts/pagination.js
+++ static/scripts/pagination.js
@@ -3,15 +3,29 @@ const nextBtn = $("#next");
 const allQuestions = $("div.questionInfo");
 let currentQuestion = 0;
 
-function showQuestion(nextQuestion) {
-    $(allQuestions[currentQuestion]).removeClass("questionInfo");
-    $(allQuestions[nextQuestion]).addClass("nextQuestion");
-    currentQuestion = nextQuestion;
+// Adding the class to start the pagination
+$(allQuestions[0]).addClass("nextQuestion");
 
+function showQuestion(nextQuestion) {
+    $(allQuestions[currentQuestion]).removeClass("nextQuestion");
+    $(allQuestions[nextQuestion]).addClass("nextQuestion");
+    
     // The first question should not have a previous button
     currentQuestion === 0 ? previousBtn.hide() : previousBtn.show();
-
+    
     // The last question should not have a next button 
     currentQuestion === allQuestions.length - 1 ? nextBtn.hide() : nextBtn.show();
-
+    
+    currentQuestion = nextQuestion;
 }
+
+function showNextQuestion() {
+    showQuestion(currentQuestion + 1);
+}
+
+function showPreviousQuestion() {
+    showQuestion(currentQuestion - 1);
+}
+
+$(nextBtn).on("click", showNextQuestion);
+$(previousBtn).on("click", showPreviousQuestion);
