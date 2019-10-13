e909a4a37650929cdf812b9b083e9824adf52456
lordviet <viet1231@abv.bg>
2019-10-14 02:27:07 +0300

    added pagination styles

--- static/scripts/pagination.js
+++ static/scripts/pagination.js
@@ -3,6 +3,8 @@ const nextBtn = $("#next");
 const allQuestions = $("div.questionInfo");
 let currentQuestion = 0;
 
+console.log(allQuestions.length)
+
 // Adding the class to start the pagination
 $(allQuestions[0]).addClass("nextQuestion");
 
@@ -10,13 +12,14 @@ function showQuestion(nextQuestion) {
     $(allQuestions[currentQuestion]).removeClass("nextQuestion");
     $(allQuestions[nextQuestion]).addClass("nextQuestion");
     
+    currentQuestion = nextQuestion;
+    
     // The first question should not have a previous button
     currentQuestion === 0 ? previousBtn.hide() : previousBtn.show();
     
     // The last question should not have a next button 
-    currentQuestion === allQuestions.length - 1 ? nextBtn.hide() : nextBtn.show();
+    currentQuestion === (allQuestions.length - 1) ? nextBtn.hide() : nextBtn.show();
     
-    currentQuestion = nextQuestion;
 }
 
 function showNextQuestion() {
