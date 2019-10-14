cc3190ac08b82f2158163f54b0c3273ce07f109f
superfan <trendemil@gmail.com>
2019-10-14 15:14:33 +0300

    added server error handling

--- index.js
+++ index.js
@@ -9,5 +9,9 @@ dbConnector().then(() => {
     require("./config/express")(app);
     require("./config/routes")(app);
 
+    app.use(function(err, req, res, next) {
+        res.render('500.hbs', { errorMessage: err.message });
+    })
+
     app.listen(config.port, console.log(`Listening on port ${config.port}`));
 }).catch(console.error);
